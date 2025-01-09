3389e431f4fe83764b947d12ec19451c915365da
Ludovico Paris <ludovicoparis1997@gmail.com>
2025-01-09 17:46:43 +0100

    updated server message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, DevOps World!');
+  res.end('Hello, DevOps World ! :)');
 });
 
 server.listen(PORT, () => {
